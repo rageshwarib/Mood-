7acb01d600839061d1191e751d6c9562f9ef77bb
rageshwari <ragu.bodke@gmail.com>
2020-04-19 23:53:11 +0530

    ADDED:Code to invoke method using reflection

--- src/main/java/com/BridgeLabz/moodAnalyser/MoodAnalyserFactory.java
+++ src/main/java/com/BridgeLabz/moodAnalyser/MoodAnalyserFactory.java
@@ -2,22 +2,32 @@ package com.BridgeLabz.moodAnalyser;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MoodAnalyserFactory {
     public static Constructor<?> getConstructor(String className, Class<?>... parameter) throws MoodAnalysisException {
         try {
             Class<?> moodAnalyserClass = Class.forName(className);
             return moodAnalyserClass.getConstructor(parameter);
-        }
-        catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.CLASS_NOT_FOUND, "Class not found");
-        }
-        catch (NoSuchMethodException e) {
+        } catch (NoSuchMethodException e) {
             throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Method not found");
         }
     }
+
     public static MoodAnalyser createMoodAnalyserObject(Constructor<?> moodAnalyserConstructor, Object... message) throws IllegalAccessException, InvocationTargetException, InstantiationException, InvocationTargetException {
-        return (MoodAnalyser)moodAnalyserConstructor.newInstance(message);
+        return (MoodAnalyser) moodAnalyserConstructor.newInstance(message);
     }
 
+    public static Object invokeMethod(Object moodAnalyserObject, String analyseMood) throws MoodAnalysisException, IllegalAccessException, InvocationTargetException {
+        try {
+            Class objectClass = moodAnalyserObject.getClass();
+            Method moodMethod = objectClass.getMethod(analyseMood);
+            Object result = moodMethod.invoke(moodAnalyserObject);
+            return result;
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Method not found");
+        }
+    }
 }
